8825ba7631ab47eec15e038e76652bfd34966964
Rey95-pixel <152308239+Rey95-pixel@users.noreply.github.com>
2025-01-29 15:10:28 +0800

    Add files via upload

---
+++ light.js
@@ -0,0 +1,15 @@
+btnOn.onclick = on;
+btnOff.onclick = off;
+
+function on(){
+ document.getElementById('light').src = "lightOn.png";
+}
+
+function off(){
+    document.getElementById('light').src = "lightOff.png";
+}
+
+
+
+
+
